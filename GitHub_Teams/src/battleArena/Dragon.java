c811439e448d7c4223b9ca6f1c31f701aebf9447
NoahSchennach <noah.schennach@hak-reutte.ac.at>
2024-02-19 11:45:24 +0100

    added Metohds and constructor to Dragon

--- GitHub_Teams/src/battleArena/Dragon.java
+++ GitHub_Teams/src/battleArena/Dragon.java
@@ -1,5 +1,37 @@
 package battleArena;
 
-public class Dragon {
+import java.util.concurrent.ThreadLocalRandom;
 
+public class Dragon extends Characters {
+
+	private boolean fly;
+
+	public Dragon(String name, int hp, boolean specialAbilityActive) {
+		super(name, specialAbilityActive);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void getDamage(int hp) {
+		this.setHp(this.getHp()-hp);
+		
+	}
+
+	@Override
+	public void attack(Character enemy) {
+		int attackValue = ThreadLocalRandom.current().nextInt(20, 25 +1);
+		
+		if(flyActive() == true) {
+			attackValue -= ThreadLocalRandom.current().nextInt(5, 10 +1);
+		 this.hp += 10;
+			
+		}
+		
+	}
+
+	public void flyActive(boolean fly, int hp) {
+		
+	}
+	
+	
 }
