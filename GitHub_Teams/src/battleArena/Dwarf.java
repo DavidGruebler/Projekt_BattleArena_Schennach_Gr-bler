41f15ad1c56831fff703f5c93393277fe707a53a
DavidGruebler <gruebler@mail.de>
2024-02-19 11:45:41 +0100

    Added getter and setter

--- GitHub_Teams/src/battleArena/Dwarf.java
+++ GitHub_Teams/src/battleArena/Dwarf.java
@@ -1,13 +1,15 @@
 package battleArena;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Dwarf extends Characters {
 	
 	private boolean drawfClout;
 
-	public Dwarf(String name, int hp, boolean specialAbilityActive) {
-		super(name, hp, specialAbilityActive);
-		// TODO Auto-generated constructor stub
-	}
+	public Dwarf(String name, int hp, boolean specialAbilityActive, boolean drawClout) {
+		super(name, specialAbilityActive);
+		this.drawfClout = drawfClout;
+			}
 	
 	
 	public boolean isDrawfClout() {
@@ -20,13 +22,14 @@ public class Dwarf extends Characters {
 	
 	@Override
 	public void getDamage(int hp) {
-		
+		this.setHp
 		
 	}
 
 	@Override
 	public void attack(Character enemy) {
-		// TODO Auto-generated method stub
+		int damage = (ThreadLocalRandom.current().nextInt(15, 25 + 1));
+		enemy = this.setHp() - damage;
 		
 	}
 }
