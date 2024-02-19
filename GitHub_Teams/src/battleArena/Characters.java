f6871b64c301c4a682413e715ffa14335e94ddb1
DavidGruebler <gruebler@mail.de>
2024-02-19 11:09:42 +0100

    Updated the abstract class Character

--- GitHub_Teams/src/battleArena/Characters.java
+++ GitHub_Teams/src/battleArena/Characters.java
@@ -1,5 +1,44 @@
 package battleArena;
 
-public class Characters {
+public abstract class Characters {
+	
+	private String name;
+	private int hp;
+	private boolean specialAbilityActive;
+	
+	public Characters(String name, int hp, boolean specialAbilityActive) {
+		super();
+		this.name = name;
+		this.hp = hp;
+		this.specialAbilityActive = specialAbilityActive;
+	}
+	
+	public  abstract void getDamage(int hp);
+	
+	public abstract void attack(Character enemy);
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getHp() {
+		return hp;
+	}
+	public void setHp(int hp) {
+		this.hp = hp;
+	}
+	public boolean isSpecialAbilityActive() {
+		return specialAbilityActive;
+	}
+	public void setSpecialAbilityActive(boolean specialAbilityActive) {
+		this.specialAbilityActive = specialAbilityActive;
+	}
 
+	@Override
+	public String toString() {
+		return "Characters [name=" + name + ", hp=" + hp + ", specialAbilityActive=" + specialAbilityActive + "]";
+	}
+	
 }
