237868718a935edff4393bcdbdc01d44bd74bf25
DavidGruebler <gruebler@mail.de>
2024-02-19 10:37:40 +0100

    Created all classes

---
+++ GitHub_Teams/src/battleArea/BattleArea.java
@@ -0,0 +1,5 @@
+package battleArea;
+
+public class BattleArea {
+
+}
--- GitHub_Teams/src/battleArea/Test.java
+++ GitHub_Teams/src/battleArea/Test.java
@@ -3,7 +3,7 @@ package battleArea;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("HI");
+		// TODO Auto-generated method stub
 
 	}
 
