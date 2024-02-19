b35585122f42dfe7dd0d8d487ee39bae4c97e55a
DavidGruebler <gruebler@mail.de>
2024-02-19 11:30:21 +0100

    Made changes

--- GitHub_Teams/src/battleArena/Dwarf.java
+++ GitHub_Teams/src/battleArena/Dwarf.java
@@ -1,5 +1,32 @@
 package battleArena;
 
-public class Dwarf {
+public class Dwarf extends Characters {
+	
+	private boolean drawfClout;
 
+	public Dwarf(String name, int hp, boolean specialAbilityActive) {
+		super(name, hp, specialAbilityActive);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	public boolean isDrawfClout() {
+		return drawfClout;
+	}
+
+	public void setDrawfClout(boolean drawfClout) {
+		this.drawfClout = drawfClout;
+	}
+	
+	@Override
+	public void getDamage(int hp) {
+		
+		
+	}
+
+	@Override
+	public void attack(Character enemy) {
+		// TODO Auto-generated method stub
+		
+	}
 }
